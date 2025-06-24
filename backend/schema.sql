9f48662e0914f1a8375b4a5ad0ca943dcd5da84f
samclintonb <clintonsam05@gmail.com>
2025-06-24 17:28:08 +0100

    updated schema.spl (myself)

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,11 +1,8 @@
-CREATE TABLE  clients (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    email TEXT NOT NULL,
-    phone TEXT NOT NULL
+CREATE TABLE clients 
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  phone TEXT,
+  company TEXT
 );
-
-INSERT INTO clients (name, email, phone) VALUES
-('Alice Johnson', 'alice@example.com', '1234567890'),
-('Bob Smith', 'bob@example.com', '9876543210'),
-('Carol Davis', 'carol@example.com', '5551234567');
